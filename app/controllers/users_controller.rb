03f18596be0a2b624d6fd2c8e7b39b39831e5e85
MK <publicelo@yahoo.com.au>
2022-09-10 17:37:58 +0200

    AddingControllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.mail_address = Random
+    @user.password = "password"
+    @user.save
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :avatar_url, :birth_date, :address, :latitude, :longitude, :mail_address, :password, :description, :music_style, :goal, :experience_in_years, :number_of_concerts, :disponibility)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users..."
+user1 = User.new(
+  name: "Matthis",
+  surname: "Savagnac",
+  email: "test@test.fr",
+  encrypted_password: "123456"
+)
+user1.save!
+puts "-> #{user1.name} has been created"
+
+user2 = User.new(
+  name: "Fabrice",
+  surname: "Sauvion",
+  email: "fabrice@gmail.fr",
+  encrypted_password: "123456"
+)
+user2.save!
+puts "-> #{user2.name} has been created"
+
+user3 = User.new(
+  name: "Jeanne",
+  surname: "Blear",
+  email: "jeanne@gmail.fr",
+  encrypted_password: "123456"
+)
+user3.save!
+puts "-> #{user3.name} has been created"
+
+puts "Finished!"
