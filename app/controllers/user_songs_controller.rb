8a1cd21266ea8b5c7811bf6a6a6083770fdb00ea
Raphael Zimeray <raphaelzimeray@me.com>
2022-09-13 21:20:31 +0200

    controllers and action cable

--- app/controllers/user_songs_controller.rb
+++ app/controllers/user_songs_controller.rb
@@ -1,2 +1,42 @@
 class UserSongsController < ApplicationController
+
+  def index
+    @user_songs = UserSong.all
+  end
+
+  def show
+    @user_song = UserSong.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @user_song = UserSong.new
+  end
+
+  def create
+    @user_song = UserSong.new(user_song_params)
+    @user_song.user_id = current_user.id
+    if @user_song.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user_song = UserSong.find(params[:id])
+  end
+
+
+  def update
+    @user_song.update(user_song_params)
+  end
+
+
+  private
+
+  def user_song_params
+    user_song.require(:user_song).permit(:title, :url, :category)
+  end
+
 end
