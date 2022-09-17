227f73788b35d381ab07708d994aa3f2694463e0
Matthis Savagnac <msavagnac@gmail.com>
2022-09-17 13:14:29 +0200

    started with acts as favoritor gem, ended up doinc the front for users index aswell

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
     resources :user_songs, only: [:index, :new, :create, :show, :destroy]
     resources :user_instruments, only: [:new, :create, :edit, :update, :destroy]
   end
+
+  resources :users, only: :index do
+    member do
+      post 'toggle_favorite', to: "users#toggle_favorite"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
