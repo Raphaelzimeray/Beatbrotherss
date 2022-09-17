70dc446b60a51bae39bb0615bc73a0e0307d7521
Marine Sourin <marine.sourin@swile.co>
2022-09-17 18:13:22 +0200

    Add ActionCable / Chatrooms + front

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
