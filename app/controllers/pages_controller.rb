9953fb03b5c7230e26783c1f9e5b85f638343a9d
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-24 19:49:19 +0100

    nothing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @body_class = 'homepage-background'
     @user = User.new
   end
-  
+
 end
